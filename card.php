b0b1e8b0e0e70c1e83ce72af531cdf897a3bd18f
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-06 18:04:43 +0700

    Create card.php

---
+++ card.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-4">
+                <div class="card" style="width: 100%;">
+                    <img class="card-img-top" src="img/work/work1.jpg" alt=" Card image cap">
+                    <div class="card-body">
+
+                        <div class="card-title">
+                            HELLOjaa
+                        </div>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <div class="card-footer">By Boon HAHA</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card" style="width: 100%;">
+                    <img class="card-img-top" src="img/work/work2.jpg" alt=" Card image cap">
+                    <div class="card-body">
+
+                        <div class="card-title">
+                            HELLOKAA
+                        </div>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <div class="card-footer">By Boon</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="card" style="width: 100%;">
+                    <img class="card-img-top" src="img/work/work3.jpg" alt=" Card image cap">
+                    <div class="card-body">
+
+                        <div class="card-title">
+                            HELLOAgain
+                        </div>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                        <div class="card-footer">By Boon Yanisa</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
