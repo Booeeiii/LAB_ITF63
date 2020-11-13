3559a9400dbb53bc755fa01b565e2d61d6480d81
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-13 22:16:26 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,5 @@
 <?php
 $conn = mysqli_init();
-include('show (2).php');
 mysqli_real_connect($conn, 'labitfyanisa.mysql.database.azure.com', 'booeeiii@labitfyanisa', '16398booN', 'Ynaisa', 3306);
 if (mysqli_connect_errno($conn))
 {
@@ -8,20 +7,6 @@ if (mysqli_connect_errno($conn))
 }
 
 
-$name = $_POST['name'];
-$comment = $_POST['comment'];
-$link = $_POST['link'];
 
-
-$sql = "DELETE FROM guestbook WHERE name="$name"";
-
-
-if (mysqli_query($conn, $sql)) {
-    echo "Deleted successfully";
-    #echo "window.location = "show (2).php";";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
 mysqli_close($conn);
 ?>
