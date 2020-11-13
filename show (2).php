43c8d697305a98544d733f2087a0e44fa5e14f7d
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-13 17:16:30 +0700

    Add files via upload

--- show (2).php
+++ show (2).php
@@ -34,5 +34,14 @@ while($Result = mysqli_fetch_array($res))
 <?php
 mysqli_close($conn);
 ?>
+<form action = "insert.php" method = "post" id="CommentForm" >
+    Name:<br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+    Link:<br>
+    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+    <input type="submit" id="commentBtn">
+  </form>
 </body>
 </html>
