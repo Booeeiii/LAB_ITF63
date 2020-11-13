1d3f50538328b0384d1ed78052c64febff811637
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-13 23:11:07 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -6,10 +6,10 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$name = $_REQUEST['Name'];
+$name_del = $_REQUEST['name'];
 
 
-$sql = "DELETE FROM guestbook WHERE Name=$name";
+$sql = "DELETE FROM guestbook WHERE Name='$name_del'";
 
 if (mysqli_query($conn, $sql)) {
     echo "Deleted successfully";
