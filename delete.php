cbc567359de6f86c14190a23ef0da68dc1f9efd5
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-17 17:16:21 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,6 @@
-<meta charset="UTF-8">
+
 <?php
+<meta charset="UTF-8">
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labitfyanisa.mysql.database.azure.com', 'booeeiii@labitfyanisa', '16398booN', 'Ynaisa', 3306);
 if (mysqli_connect_errno($conn))
@@ -7,16 +8,17 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$name_del = $_REQUEST['Name'];
+$name = $_POST['Name'];
 
 
-$sql = "DELETE FROM guestbook WHERE Name='$name_del'";
+$sql = "DELETE FROM guestbook WHERE Name='$name'";
 
 if (mysqli_query($conn, $sql)) {
     echo "Deleted successfully";
   } 
 else {
-    echo "Error: "  }
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
   
 
 mysqli_close($conn);
