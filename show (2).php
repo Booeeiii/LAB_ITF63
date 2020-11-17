bc2fa6576efe4dec8468a0004ce19158f6a33e2b
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-17 16:35:11 +0700

    Update show (2).php

--- show (2).php
+++ show (2).php
@@ -29,6 +29,7 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
+	<td><?php echo $Result['Id'];?></div></td>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
