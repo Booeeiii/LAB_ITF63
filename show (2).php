31dac05a43b12b8ab0640e2af4946bdc0f9b9c6a
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-17 16:38:09 +0700

    Update show (2).php

--- show (2).php
+++ show (2).php
@@ -19,6 +19,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 	<table class="table table-hoverable"width="600" border="1" bgcolor="pink" bordercolorlight="white">
 	
   <tr>
+	  <th width="100"> <div align="center">id</div></th>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
     <th width="150"> <div align="center">Link </div></th>
@@ -29,7 +30,7 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-	<td><?php echo $Result['Id'];?></div></td>
+	<td><?php echo $Result['id'];?></div></td>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
