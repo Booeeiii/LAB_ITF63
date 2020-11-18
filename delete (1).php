3e1930dcfb60db0c0fa6e5945c493621a41fa8ba
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 17:24:08 +0700

    Update delete (1).php

--- delete (1).php
+++ delete (1).php
@@ -7,6 +7,18 @@ if (mysqli_connect_errno($conn))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
+$ID = $_POST['ID'];
+$sql = "DELETE FROM guestbook WHERE ID='$ID'";
+
+if (mysqli_query($conn, $sql)) {
+    echo '<div class="container">
+            <h3>Comment has been deleted successfully.</h3>
+            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
+         </div>';
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
 mysqli_close($conn);
 
 ?>
