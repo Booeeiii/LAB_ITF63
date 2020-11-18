edba5f4950cfed59f0fcb8f19f49aefbcde1fdb4
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 17:29:33 +0700

    Update delete (1).php

--- delete (1).php
+++ delete (1).php
@@ -13,7 +13,7 @@ $sql = "DELETE FROM guestbook WHERE ID='$ID'";
 if (mysqli_query($conn, $sql)) {
     echo '<div class="container">
             <h3>Comment has been deleted successfully.</h3>
-            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
+            <a role="button" class="btn btn-primary mt-3" href="show (2).php">Back To Home</a>
          </div>';
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
