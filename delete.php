78fce8bc3725ddf9a2023cad47499a0ca9962fd7
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 16:08:05 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -1,25 +1,34 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITF Database Lab</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 <?php
-<meta charset="UTF-8">
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'labitfyanisa.mysql.database.azure.com', 'booeeiii@labitfyanisa', '16398booN', 'Ynaisa', 3306);
-if (mysqli_connect_errno($conn))
+$dbconfig = include('dbconfig.php');
+$conn = mysqli_connect($dbconfig['host'], $dbconfig['username'], $dbconfig['password'], $dbconfig['database']);
+if (!$conn)
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-$name = $_POST['Name'];
-
-
-$sql = "DELETE FROM guestbook WHERE Name='$name'";
+$ID = $_POST['ID'];
+$sql = "DELETE FROM guestbook WHERE ID='$ID'";
 
 if (mysqli_query($conn, $sql)) {
-    echo "Deleted successfully";
-  } 
-else {
+    echo '<div class="container">
+            <h3>Comment has been deleted successfully.</h3>
+            <a role="button" class="btn btn-primary mt-3" href="guestbook.php">Home</a>
+         </div>';
+} else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
+}
   
-
 mysqli_close($conn);
 ?>
+</body>
+</html>
