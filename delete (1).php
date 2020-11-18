e2908e7961bce8a31f5b54279714744a3717f2db
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 19:56:09 +0700

    Update delete (1).php

--- delete (1).php
+++ delete (1).php
@@ -1,5 +1,6 @@
-<?php
 <html>
+<?php
+
    <head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
@@ -24,5 +25,6 @@ if (mysqli_query($conn, $sql)) {
 }
 
 mysqli_close($conn);
-</html>
+
 ?>
+</html>
