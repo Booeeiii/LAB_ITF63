12e0848e6d8cace96046fcd44649419b75755dd1
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 16:13:59 +0700

    Update show (2).php

--- show (2).php
+++ show (2).php
@@ -32,7 +32,10 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><?php echo $Result['Link'];?></td>
-	<td><?php echo "<a href='delete (1).php'>del</a>"?></td>;
+	<form action="delete (1).php" method="post" class="d-inline">
+                            <input type="hidden" name="ID" value=<?php echo $row['ID'];?>>
+                            <button type="submit" class="btn btn-sm btn-danger mb-1">Delete</button>
+                        </form>
 		<input type="button" name = "name" id="idName" value="แก้ไข" onClick="DELETE FROM guestbook WHERE Name=$Result['Name']"></a></td>
   </tr>
 <?php
