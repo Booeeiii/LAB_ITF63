d317a0fc33c54257aac72da079ad0c71752cceff
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 18:08:59 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -9,9 +9,9 @@
 </head>
 <body>
 <?php
-$dbconfig = include('dbconfig.php');
-$conn = mysqli_connect($dbconfig['host'], $dbconfig['username'], $dbconfig['password'], $dbconfig['database']);
-if (!$conn)
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labitfyanisa.mysql.database.azure.com', 'booeeiii@labitfyanisa', '16398booN', 'Ynaisa', 3306);
+if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
