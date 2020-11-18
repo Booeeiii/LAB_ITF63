ae98ead587ae59a17f8d5bfaa96234f046fd0ab0
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 20:23:58 +0700

    Update insert (1).php

--- insert (1).php
+++ insert (1).php
@@ -1,3 +1,12 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITF Lab</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 <?php
 
 $conn = mysqli_init();
@@ -17,7 +26,10 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo <div class="container mt-4">
+        <h3>"New record created successfully"</h3>
+        <a role="button" class="btn btn-outline-secondary" href="show (2).php">Back to Home</a>
+        </div>
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
@@ -25,3 +37,4 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);
 
 ?>
+</htmll>
