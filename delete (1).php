386d3f5b2226188cba588c7b3c4679070c2953f7
Booeeiii <73687103+Booeeiii@users.noreply.github.com>
2020-11-18 20:01:16 +0700

    Update delete (1).php

--- delete (1).php
+++ delete (1).php
@@ -1,10 +1,13 @@
 <html>
-<?php
-
    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ITF Lab</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
   </head>
+<body>
+<?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labitfyanisa.mysql.database.azure.com', 'booeeiii@labitfyanisa', '16398booN', 'Ynaisa', 3306);
 if (mysqli_connect_errno($conn))
@@ -27,4 +30,5 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);
 
 ?>
+</body>
 </html>
